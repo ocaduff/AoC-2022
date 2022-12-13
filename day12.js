0e555ba33f438590083780ca71d05546c89519b9
ocaduff <ocaduff@gmail.com>
2022-12-13 01:49:42 +0100

    Update day12.js
    Day 12 Part 2

--- day12.js
+++ day12.js
@@ -6,7 +6,7 @@ accszExk
 acctuvwj
 abdefghi`;
 
-let traverse = input => {
+let traverse = (input, startingChar) => {
   const map = input.split("\n").map(line => line.split(""));
   // visitedMap contains minimum path length up to this field seen up to the current visit
   const visitedMap = Array.from({ length: map.length}, () => Array.from({ length: map[0].length}, () => 9999999));
@@ -54,7 +54,7 @@ let traverse = input => {
     const currentPos = path[path.length - 1];
     if (isBestSignalPosition(currentPos)) {
       if (path.length < currentShortestPath) {
-        console.log("found path with " + (path.length - 1) + " steps");
+        //console.log("found path with " + (path.length - 1) + " steps");
         currentShortestPath = path.length - 1;
       }
       return;
@@ -72,26 +72,31 @@ let traverse = input => {
       findPeak(path);
       path.pop();
     }
+    return currentShortestPath;
   }
 
   
   let printSurrounding = (x,y) => console.log([visitedMap[x][y],visitedMap[x-1][y],visitedMap[x+1][y],visitedMap[x][y-1],visitedMap[x][y+1]]);
 
   let find = (value) => {
+    const results = [];
     for (let x = 0; x < map.length; x++) {
       const y = map[x].indexOf(value);
       if (y >= 0) {
-        return { x, y };
+        results.push({ x, y });
       }
     }
+    return results;
   }
 
-  const start = find("S");
-  const path = [start];
-  findPeak(path);
-  console.log("done");
+  find(startingChar).map(startingPoint => findPeak([startingPoint]));
+  console.log("done. shortestPath: " + currentShortestPath);
 }
 
-traverse(sampleInput);
+traverse(sampleInput, "S");
 let puzzleInput = document.getElementsByTagName("pre")[0].textContent.trimRight();
-traverse(puzzleInput);
+traverse(puzzleInput, "S");
+
+// Part 2
+traverse(sampleInput, "a");
+traverse(puzzleInput, "a");
